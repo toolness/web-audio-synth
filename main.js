50319a5b3de9c0cc8e5b2a8896a6068b27386265
Atul Varma <varmaa@gmail.com>
2016-07-26 22:53:26 -0400

    make duty cycle configurable.

--- main.js
+++ main.js
@@ -27,28 +27,54 @@ function stop() {
 class SquareWave {
   constructor(sampleRate) {
     this.sampleRate = sampleRate;
-    this.freq = 220;
+    this._dutyCycle = 0.5;
+    this._freq = 220;
+    this._recompute();
   }
 
   set freq(hz) {
     this._freq = hz;
-    this._samplesPerSignalFlip = Math.floor((this.sampleRate / hz) / 2);
+    this._recompute();
   }
 
   get freq() {
     return this._freq;
   }
 
+  set dutyCycle(ratio) {
+    this._dutyCycle = ratio;
+    this._recompute();
+  }
+
+  get dutyCycle() {
+    return this._dutyCycle;
+  }
+
+  _recompute() {
+    let period = Math.floor(this.sampleRate / this._freq);
+
+    this._highSamplesPerPeriod = Math.floor(period * this._dutyCycle);
+    this._lowSamplesPerPeriod = period - this._highSamplesPerPeriod;
+  }
+
   signal() {
+    const SIGNAL_LOW = -1;
+    const SIGNAL_HIGH = 1;
+
     let self = this;
-    let signal = 1;
-    let samplesUntilSignalFlip = 0;
+    let signal = SIGNAL_LOW;
+    let samplesUntilSignalFlip = self._lowSamplesPerPeriod;
 
     function *signalGenerator() {
       while (true) {
         if (samplesUntilSignalFlip == 0) {
-          signal = -signal;
-          samplesUntilSignalFlip = self._samplesPerSignalFlip;
+          if (signal === SIGNAL_LOW) {
+            signal = SIGNAL_HIGH;
+            samplesUntilSignalFlip = self._highSamplesPerPeriod;
+          } else {
+            signal = SIGNAL_LOW;
+            samplesUntilSignalFlip = self._lowSamplesPerPeriod;
+          }
         } else {
           samplesUntilSignalFlip--;
         }

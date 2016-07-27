1483589610b16fb81eac6ebb8588e9ebcc21a06d
Atul Varma <varmaa@gmail.com>
2016-07-26 22:29:56 -0400

    more es6 refactoring

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
+"use strict";
+
 let audioCtx = new AudioContext();
 let source = audioCtx.createBufferSource();
 let scriptNode = audioCtx.createScriptProcessor(1024, 1, 1);
-let signal = 1;
-let samplesUntilSignalFlip = 0;
-let samplesPerSignalFlip;
-let currFreq = 220;
 
 source.connect(scriptNode);
 scriptNode.connect(audioCtx.destination);
 source.start();
-freq(currFreq);
-
-setInterval(() => {
-  currFreq += 1;
-  freq(currFreq);
-}, 5);
 
 scriptNode.onaudioprocess = e => {
   let outputBuffer = e.outputBuffer;
@@ -23,13 +15,7 @@ scriptNode.onaudioprocess = e => {
     let data = e.outputBuffer.getChannelData(chan);
 
     for (let i = 0; i < e.outputBuffer.length; i++) {
-      if (samplesUntilSignalFlip == 0) {
-        signal = -signal;
-        samplesUntilSignalFlip = samplesPerSignalFlip;
-      } else {
-        samplesUntilSignalFlip--;
-      }
-      data[i] = signal;
+      data[i] = squareWaveSignal.next().value;
     }
   }
 };
@@ -38,6 +24,45 @@ function stop() {
   scriptNode.disconnect(audioCtx.destination);
 }
 
-function freq(hz) {
-  samplesPerSignalFlip = Math.floor((audioCtx.sampleRate / hz) / 2);
+class SquareWave {
+  constructor(sampleRate) {
+    this.sampleRate = sampleRate;
+    this.freq = 220;
+  }
+
+  set freq(hz) {
+    this._freq = hz;
+    this._samplesPerSignalFlip = Math.floor((this.sampleRate / hz) / 2);
+  }
+
+  get freq() {
+    return this._freq;
+  }
+
+  signal() {
+    let self = this;
+    let signal = 1;
+    let samplesUntilSignalFlip = 0;
+
+    function *signalGenerator() {
+      while (true) {
+        if (samplesUntilSignalFlip == 0) {
+          signal = -signal;
+          samplesUntilSignalFlip = self._samplesPerSignalFlip;
+        } else {
+          samplesUntilSignalFlip--;
+        }
+        yield signal;
+      }
+    }
+
+    return signalGenerator();
+  };
 }
+
+let squareWave = new SquareWave(audioCtx.sampleRate);
+let squareWaveSignal = squareWave.signal();
+
+setInterval(() => {
+  squareWave.freq += 1;
+}, 5);

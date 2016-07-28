27da26dfec10e297f5b65585f9774e98123f0fcd
Atul Varma <varmaa@gmail.com>
2016-07-27 23:06:48 -0400

    factor out square-wave.js.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 let audioCtx = new AudioContext();
 let source = audioCtx.createBufferSource();
 let scriptNode = audioCtx.createScriptProcessor(1024, 1, 1);
+let squareWave = new SquareWave(audioCtx.sampleRate);
+let squareWaveSignal = squareWave.signal();
 
 source.connect(scriptNode);
 scriptNode.connect(audioCtx.destination);
@@ -23,71 +25,6 @@ scriptNode.onaudioprocess = e => {
 function stop() {
   scriptNode.disconnect(audioCtx.destination);
 }
-
-class SquareWave {
-  constructor(sampleRate) {
-    this.sampleRate = sampleRate;
-    this._dutyCycle = 0.5;
-    this._freq = 220;
-    this._recompute();
-  }
-
-  set freq(hz) {
-    this._freq = hz;
-    this._recompute();
-  }
-
-  get freq() {
-    return this._freq;
-  }
-
-  set dutyCycle(ratio) {
-    this._dutyCycle = ratio;
-    this._recompute();
-  }
-
-  get dutyCycle() {
-    return this._dutyCycle;
-  }
-
-  _recompute() {
-    let period = Math.floor(this.sampleRate / this._freq);
-
-    this._highSamplesPerPeriod = Math.floor(period * this._dutyCycle);
-    this._lowSamplesPerPeriod = period - this._highSamplesPerPeriod;
-  }
-
-  signal() {
-    const SIGNAL_LOW = -1;
-    const SIGNAL_HIGH = 1;
-
-    let self = this;
-    let signal = SIGNAL_LOW;
-    let samplesUntilSignalFlip = self._lowSamplesPerPeriod;
-
-    function *signalGenerator() {
-      while (true) {
-        if (samplesUntilSignalFlip == 0) {
-          if (signal === SIGNAL_LOW) {
-            signal = SIGNAL_HIGH;
-            samplesUntilSignalFlip = self._highSamplesPerPeriod;
-          } else {
-            signal = SIGNAL_LOW;
-            samplesUntilSignalFlip = self._lowSamplesPerPeriod;
-          }
-        } else {
-          samplesUntilSignalFlip--;
-        }
-        yield signal;
-      }
-    }
-
-    return signalGenerator();
-  };
-}
-
-let squareWave = new SquareWave(audioCtx.sampleRate);
-let squareWaveSignal = squareWave.signal();
 
 setInterval(() => {
   squareWave.freq += 1;
---
+++ square-wave.js
@@ -0,0 +1,61 @@
+class SquareWave {
+  constructor(sampleRate) {
+    this.sampleRate = sampleRate;
+    this._dutyCycle = 0.5;
+    this._freq = 220;
+    this._recompute();
+  }
+
+  set freq(hz) {
+    this._freq = hz;
+    this._recompute();
+  }
+
+  get freq() {
+    return this._freq;
+  }
+
+  set dutyCycle(ratio) {
+    this._dutyCycle = ratio;
+    this._recompute();
+  }
+
+  get dutyCycle() {
+    return this._dutyCycle;
+  }
+
+  _recompute() {
+    let period = Math.floor(this.sampleRate / this._freq);
+
+    this._highSamplesPerPeriod = Math.floor(period * this._dutyCycle);
+    this._lowSamplesPerPeriod = period - this._highSamplesPerPeriod;
+  }
+
+  signal() {
+    const SIGNAL_LOW = -1;
+    const SIGNAL_HIGH = 1;
+
+    let self = this;
+    let signal = SIGNAL_LOW;
+    let samplesUntilSignalFlip = self._lowSamplesPerPeriod;
+
+    function *signalGenerator() {
+      while (true) {
+        if (samplesUntilSignalFlip == 0) {
+          if (signal === SIGNAL_LOW) {
+            signal = SIGNAL_HIGH;
+            samplesUntilSignalFlip = self._highSamplesPerPeriod;
+          } else {
+            signal = SIGNAL_LOW;
+            samplesUntilSignalFlip = self._lowSamplesPerPeriod;
+          }
+        } else {
+          samplesUntilSignalFlip--;
+        }
+        yield signal;
+      }
+    }
+
+    return signalGenerator();
+  };
+}

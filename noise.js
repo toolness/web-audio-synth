d18396e1aa156c9ac2a94adbf81b0053b4b6b5b8
Atul Varma <varmaa@gmail.com>
2016-07-30 08:43:00 -0400

    Attempt to make noise periodic.

--- noise.js
+++ noise.js
@@ -1,8 +1,32 @@
 class Noise {
+  constructor(sampleRate) {
+    this.sampleRate = sampleRate;
+    this.freq = 220;
+  }
+
+  set freq(hz) {
+    this._freq = hz;
+
+    let period = Math.floor(this.sampleRate / this._freq);
+
+    this._samples = [];
+    for (let i = 0; i < period; i++) {
+      this._samples.push(Math.random() * 2 - 1);
+    }
+  }
+
+  get freq() {
+    return this._freq;
+  }
+
   signal() {
+    let self = this;
+
     function *signalGenerator() {
       while (true) {
-        yield Math.random() * 2 - 1;
+        for (let i = 0; i < self._samples.length; i++) {
+          yield self._samples[i];
+        }
       }
     }
 

a11f972a6147407632cfb93c6df5e4610c45b18e
Atul Varma <varmaa@gmail.com>
2016-07-31 18:57:22 -0400

    Make noise by combining waves around a frequency.

--- src/signals/noise.js
+++ src/signals/noise.js
@@ -1,8 +1,29 @@
 class Noise {
+  constructor(sampleRate) {
+    this.sampleRate = sampleRate;
+  }
+
   signal() {
+    const NUM_SIGNALS = 5;
+    const BASE_FREQ = 440;
+    const FREQ_VARIANCE = 200;
+
+    let signals = [];
+
+    for (let i = 0; i < NUM_SIGNALS; i++) {
+      let wave = new SineWave(this.sampleRate);
+      wave.freq = BASE_FREQ + Math.floor(
+        (Math.random() * 2 - 1) * FREQ_VARIANCE
+      );
+      signals.push(wave.signal());
+    }
+
     function *signalGenerator() {
       while (true) {
-        yield Math.random() * 2 - 1;
+        yield signals.reduce((sum, signal) => {
+          return sum + signal.next().value / signals.length;
+        }, 0);
+//        yield Math.random() * 2 - 1;
       }
     }
 

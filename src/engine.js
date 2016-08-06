a3e34fbe00d6abc810e956d3348600386d44d60b
Atul Varma <varmaa@gmail.com>
2016-08-06 18:53:46 -0400

    Add some temporary MIDI code.

--- src/engine.js
+++ src/engine.js
@@ -16,6 +16,7 @@ class AudioEngine {
     this._scriptNode.onaudioprocess = this._processAudio.bind(this);
     this._source = null;
     this._started = false;
+    this._initMidi();
   }
 
   activate(source) {
@@ -32,6 +33,53 @@ class AudioEngine {
 
   get sampleRate() {
     return this._audioCtx.sampleRate;
+  }
+
+  _initMidi() {
+    if (navigator.requestMIDIAccess) {
+      navigator.requestMIDIAccess({
+        sysex: false
+      }).then(midi => {
+        for (let input of midi.inputs.values()) {
+          input.onmidimessage = this._onMidiMessage.bind(this);
+        }
+      }, () => {
+        console.log("MIDI failed to init.");
+      });
+    }
+  }
+
+  // https://www.w3.org/TR/webmidi/
+  // https://www.midi.org/specifications/item/table-1-summary-of-midi-message
+  _onMidiMessage(e) {
+    const NOTE_ON = 0x90;
+    const NOTE_OFF = 0x80;
+    const PROGRAM_CHANGE = 0xc0;
+
+    let [midiType, note, velocity] = e.data;
+    let programNumber = null;
+    let type = null;
+    let freq = 440 * Math.pow(2, (note - 69) / 12);
+
+    midiType = midiType & 0xf0;
+
+    if (midiType === NOTE_ON && velocity === 0) {
+      // Apparently MIDI can be weird.
+      midiType = NOTE_OFF;
+    }
+
+    if (midiType === NOTE_ON) {
+      type = 'noteon';
+    } else if (midiType === NOTE_OFF) {
+      type = 'noteoff';
+    } else if (midiType === PROGRAM_CHANGE) {
+      type = 'programchange';
+      programNumber = e.data[1];
+    }
+
+    if (type && typeof(this.onmidi) === 'function') {
+      this.onmidi({type, note, velocity, freq, programNumber});
+    }
   }
 
   _processAudio(e) {

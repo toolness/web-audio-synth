dc138c205524c4eb5c205b49b67a31025614495d
Atul Varma <varmaa@gmail.com>
2016-08-13 20:40:21 -0400

    refactor graph.js a bit.

--- src/graph.js
+++ src/graph.js
@@ -2,73 +2,88 @@
 
 /* global d3, Constant, SineWave */
 
-let translateStr = (x, y) => {
+const defaults = {
+  width: 640,
+  height: 320,
+  margin: {
+    top: 20,
+    right: 50,
+    bottom: 40,
+    left: 50
+  },
+  yDomain: [-1, 1],
+  seconds: 2
+};
+
+function translateStr(x, y) {
   return 'translate(' + x + ', ' + y + ')';
-};
+}
 
-let margin = {
-  top: 20,
-  right: 50,
-  bottom: 40,
-  left: 50
-};
-let width = 640;
-let height = 320;
+function drawGraph(selector, createSampleIterator, options) {
+  options = Object.assign({}, defaults, options);
 
-let generateData = (seconds, sampleRate, startFreq, endFreq) => {
-  let signal = new SineWave(sampleRate);
-  let data = [];
-  let freqDelta = endFreq - startFreq;
+  let margin = options.margin;
+  let width = options.width;
+  let height = options.height;
+  let seconds = options.seconds;
+  let yDomain = options.yDomain;
+  let sampleRate = Math.floor(width / seconds);
+
+  let x = d3.scaleLinear()
+    .range([0, width]);
+
+  let y = d3.scaleLinear()
+    .range([height, 0]);
+
+  let line = d3.line()
+    .x(d => x(d.x))
+    .y(d => y(d.y));
+
+  let svg = d3.select(selector)
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('transform', translateStr(margin.left, margin.top))
+    .append('g');
+
+  y.domain(yDomain);
+  x.domain([0, seconds]);
+
+  svg.append('g')
+    .attr('transform', translateStr(0, height / 2))
+    .call(d3.axisBottom(x));
+
+  svg.append('g')
+    .call(d3.axisLeft(y));
+
   let totalSamples = seconds * sampleRate;
-
-  signal.freq = new Constant(startFreq);
-
-  let samples = signal.samples();
+  let samples = createSampleIterator(sampleRate, seconds, totalSamples);
+  let data = [];
 
   for (let i = 0; i < totalSamples; i++) {
+    let nextSample = samples.next();
+    if (nextSample.done) break;
     data.push({
       x: i / sampleRate,
-      y: samples.next().value,
+      y: nextSample.value
     });
-    signal.freq.value = startFreq + freqDelta * (i / totalSamples);
   }
 
-  return data;
-};
+  svg.append('path')
+    .datum(data)
+    .attr('class', 'line')
+    .attr('d', line);
+}
 
-let x = d3.scaleLinear()
-  .range([0, width]);
+drawGraph('#graph', function *(sampleRate, seconds, totalSamples) {
+  let signal = new SineWave(sampleRate);
+  let samples = signal.samples()
+  let startFreq = 1;
+  let endFreq = 10;
+  let freqDelta = endFreq - startFreq;
 
-let y = d3.scaleLinear()
-  .range([height, 0]);
-
-let line = d3.line()
-  .x(d => x(d.x))
-  .y(d => y(d.y));
-
-let svg = d3.select('#graph')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .attr('transform', translateStr(margin.left, margin.top))
-  .append('g');
-
-let seconds = 2;
-let sampleRate = Math.floor(width / seconds);
-let startFreq = 1;
-let endFreq = 10;
-
-y.domain([-1, 1]);
-x.domain([0, seconds]);
-
-svg.append('g')
-  .attr('transform', translateStr(0, height / 2))
-  .call(d3.axisBottom(x));
-
-svg.append('g')
-  .call(d3.axisLeft(y));
-
-svg.append('path')
-  .datum(generateData(seconds, sampleRate, startFreq, endFreq))
-  .attr('class', 'line')
-  .attr('d', line);
+  for (let i = 0; i < totalSamples; i++) {
+    signal.freq.value = startFreq + freqDelta * (i / totalSamples);
+    yield samples.next().value;
+  }
+});
